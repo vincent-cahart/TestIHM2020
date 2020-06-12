e6195d4e849c84d103bc278186875b5fad77fadb
bastinz <sophie.nabitz@univ-avignon.fr>
2020-06-12 08:24:46 +0200

    Sujet du test IHM

---
+++ src/main/java/fr/univ_amu/iut/ihm/TraceurDeFonction.java
@@ -0,0 +1,40 @@
+package fr.univ_amu.iut.ihm;
+
+import fr.univ_amu.iut.utilitaires.Analyseur;
+import fr.univ_amu.iut.utilitaires.ErreurDeSyntaxe;
+
+public class TraceurDeFonction {
+
+  void calculCoeffTransformationsAffines() {
+  }
+  	
+  int transformationAffineY(double y) {
+    return 0;
+  }
+
+  int transformationAffineX(double x) {
+    return 0;
+  }
+
+  private void setIds() {
+/*      votreIdentificateur.setId("texteAAnalyser");
+      votreIdentificateur.setId("resultatAnalyse");
+      votreIdentificateur.setId("demandeAnalyser");
+      votreIdentificateur.setId("demandeTracer");
+      votreIdentificateur.setId("demandeEffacer");
+      votreIdentificateur.setId("zoneTraceCourbe");
+      votreIdentificateur.setId("choixXMin");
+      votreIdentificateur.setId("choixXMax");
+      votreIdentificateur.setId("choixEspacementX_v1");
+      votreIdentificateur.setId("choixEspacementX_v2");
+      votreIdentificateur.setId("choixEspacementY_v1");
+      votreIdentificateur.setId("choixEspacementY_v2");
+      votreIdentificateur.setId("choixCouleur");
+      votreIdentificateur.setId("choixEpaisseur");
+      votreIdentificateur.setId("segmentsATracer");
+      votreIdentificateur.setId("quadrillage");
+      votreIdentificateur.setId("axeX");
+      votreIdentificateur.setId("axeY");*/
+  }
+
+}
